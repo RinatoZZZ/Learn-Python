394dc42746b16d709f1aa566ac17f732d8061702
Rinat Khamzin <reno237@mail.ru>
2022-09-06 22:27:34 +0300

    if_1

---
+++ 1_if.py
@@ -0,0 +1,12 @@
+def age_status(age_people):
+    if 3 <= age_people < 7:
+        print('Учиться в детском саду')
+    elif 7 <= age_people <= 16:
+        print('Учиться в школе')
+    elif 17 <= age_people <= 23:
+        print('Учится в универе')
+    else:
+        print('Работает')
+
+age = int(input('Введите возраст'))
+age_status(age)
